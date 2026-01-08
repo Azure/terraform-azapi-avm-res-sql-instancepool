d84ff22088aba98662534c97510190699da529da
Sujay Pillai <sujayopillai@gmail.com>
2026-01-08 09:17:26 +0000

    chore: fix linting issues and variable type specifications

--- variables.tf
+++ variables.tf
@@ -191,7 +191,36 @@ DESCRIPTION
 # Private endpoints should be configured on individual Managed Instances within the pool.
 # tflint-ignore: terraform_unused_declarations
 variable "private_endpoints" {
-  type        = map(any)
+  type = map(object({
+    name               = optional(string, null)
+    role_assignments   = optional(map(object({
+      role_definition_id_or_name             = string
+      principal_id                           = string
+      description                            = optional(string, null)
+      skip_service_principal_aad_check       = optional(bool, false)
+      condition                              = optional(string, null)
+      condition_version                      = optional(string, null)
+      delegated_managed_identity_resource_id = optional(string, null)
+      principal_type                         = optional(string, null)
+    })), {})
+    lock               = optional(object({
+      kind = string
+      name = optional(string, null)
+    }), null)
+    tags               = optional(map(string), null)
+    subnet_resource_id = string
+    private_dns_zone_group_name             = optional(string, "default")
+    private_dns_zone_resource_ids           = optional(set(string), [])
+    application_security_group_associations = optional(map(string), {})
+    private_service_connection_name         = optional(string, null)
+    network_interface_name                  = optional(string, null)
+    location                                = optional(string, null)
+    resource_group_name                     = optional(string, null)
+    ip_configurations = optional(map(object({
+      name               = string
+      private_ip_address = string
+    })), {})
+  }))
   default     = {}
   description = "Private endpoints are not supported for SQL Instance Pools. This variable is kept for AVM interface compatibility but will be ignored."
   nullable    = false
